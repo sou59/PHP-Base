f756c88b5c1ee323149a53d06d6f694e6ed10555
Matthieu Mota <matthieumota@gmail.com>
2018-05-23 11:23:09 +0200

    Add PHP base

---
+++ 01-bases/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <title>Document</title>
+</head>
+<body>
+    <!-- Ouverture des balises PHP -->
+    <?php
+        echo 'Hello world <br />';
+        // On peut utiliser les doubles quotes ou les quotes, on peut échapper les quotes dans la chaine
+        echo "l'html \"citation\" ";
+    ?>
+</body>
+</html>
