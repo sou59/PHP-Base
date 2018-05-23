effd55cecf9d0ecd6e927781adb896ab4c28fe80
Sou 59 <sz5962@gmail.com>
2018-05-23 15:34:30 +0200

    AJout ternaire dans major.php

--- 03-condition/major.php
+++ 03-condition/major.php
@@ -1,22 +1,20 @@
 <?php
-
-    $age = 26;
-    if ($age >= 18) {
-        echo "Vous pouvez entrer";
-    } elseif ($age > 16 && $age < 18){
-        echo "Vous êtes presque majeur";
-    } elseif ($age >= 14 && $age < 16){
-        echo "Vous êtes jeune";
-    } elseif ($age < 14){
-        echo "Vous êtes trop jeune";
-    } else {
-        echo "Interdit";
-    }
-?>
-<?php/*
-//BONUS
-echo $age >= 18 ? $age > 16 && $age < 18) ? "Vous êtes presque majeur" : $age >= 14 && $age < 16 ? "Vous êtes jeune" : $age < 14 ?  "Vous êtes trop jeune" : "Interdit"
-?> -->
-
+$age = 17;
+if ($age >= 18) {
+    echo 'Vous pouvez entrez';
+} else if ($age >= 16 && $age < 18) {
+    echo 'Vous êtes presque majeur';
+} else if ($age >= 14 && $age < 16) {
+    echo 'Vous êtes jeune';
+} else if ($age < 14) {
+    echo 'Vous êtes trop jeune';
+} else {
+    echo 'Interdit';
+}
+// BONUS condition en ternaire : 
+// echo $age >= 18 ? 'Vous pouvez entrez'
+//  : ($age >= 16) ? 'Vous êtes presque majeur'
+//   : (($age >= 14) ? 'Vous êtes jeune'
+//    : 'Vous êtes trop jeune');
 
 
