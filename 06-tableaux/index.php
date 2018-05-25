20e323a5d3b4ef891af3ee92eef938f7e579b54c
Matthieu Mota <matthieumota@gmail.com>
2018-05-25 12:04:56 +0200

    Array iteration

--- 06-tableaux/index.php
+++ 06-tableaux/index.php
@@ -5,7 +5,9 @@
 $people = [
     'Jean',
     'Eric',
-    'Jeanne'
+    'Jeanne',
+    'John',
+    'Quelqu\'un'
 ];
 
 echo $people; // Ne fonctionne pas
@@ -18,7 +20,14 @@ echo '</pre>';
 var_dump($people); // Debug du tableau
 
 echo $people[1]; // Affiche Eric
+echo '<br /> ----------- FOREACH ------------ <br />';
 
+// Afficher tous les prénoms du tableau
+foreach ($people as $index => $person) {
+    echo $index . ' : ' . $person . '<br />';
+}
+
+echo '----------- FIN DU FOREACH ------------ <br />';
 // Si un index est déclaré, les éléments suivants vont être auto incrémentés par rapport à cet index
 $people = [
     'Jean',
@@ -51,3 +60,17 @@ $people = [
 ];
 
 var_dump($people);
+/*
+ Ecrire la boucle foreach qui affiche le texte ci dessous :
+ Matthieu a 26 ans et est joignable au 06.00.00.00.00, (+33) 03 21 00 00 00
+ Jean a 36 ans et est joignable au 07.00.00.00.00, (+33) 03 20 00 00 00
+ */
+foreach ($people as $person) {
+    echo $person['prenom'] . ' a ' . $person['age'] . ' ans et est joignable au ';
+    echo $person['telephones']['portable'] . ', ' . $person['telephones']['fix'] . '<br />';
+    // On peut aussi parcourir tous les téléphones avec un foreach
+    foreach ($person['telephones'] as $type => $phone) {
+        echo $type .' : '. $phone .', ';
+    }
+    echo '<br />';
+}
