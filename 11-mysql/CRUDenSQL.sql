1bff89cc0db5a9aa8b1d2bfe80df22af9872edf4
Matthieu Mota <matthieumota@gmail.com>
2018-05-30 13:54:35 +0200

    SQL

---
+++ 11-mysql/CRUDenSQL.sql
@@ -0,0 +1,20 @@
+-- CECI N'EST QU'UN SUPPORT POUR LE COURS
+-- Commentaire en SQL
+-- On insère une catégorie
+INSERT INTO category (`name`) VALUES ('Film de gangsters');
+
+-- On insère plusieurs catégories
+INSERT INTO category (`name`) VALUES
+('Action'),
+('Horreur'),
+('Science-fiction'),
+('Thriller');
+
+-- Pour récupérer toutes les catégories
+SELECT * FROM category;
+
+-- On change le nom de la catégorie qui a l'id 5
+UPDATE category SET `name` = 'Documentaire' WHERE id = 5;
+
+-- Supprimer la catégorie qui a l'id 5
+DELETE FROM category WHERE id = 5;
---
+++ 11-mysql/Movie.sql
@@ -0,0 +1,8 @@
+-- Insèrer quelques films dans la bdd
+-- 1 Film de gangster : Le Parrain 1972, Scarface 1983, Les Affranchis 1990, Heat 1995
+-- 2 Action : Die Hard 1988, Demolition Man 1993, Taken 2008, Deadpool 2016, Expandable 2010
+-- 3 Horreur : Scream 1996, Vendredi 13 1980, Saw 2005, Scary Movie 2000
+-- 4 Science Fiction : Star Wars IV Un nouvel espoir 1977, Alien 1979, ET 1982, Robocop 1987
+-- 5 Thriller : The Game 1997, Sixième Sens 1999, Usual Suspects 1995, Fight Club 1999,
+-- Inception 2010
+
