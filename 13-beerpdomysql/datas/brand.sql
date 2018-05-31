5efddc874ea78d537542a457d502a2f049d1e1de
Matthieu Mota <matthieumota@gmail.com>
2018-05-31 11:45:59 +0200

    Beer datas

---
+++ 13-beerpdomysql/datas/brand.sql
@@ -0,0 +1,7 @@
+-- Ce fichier nous permettra d'ajouter des marques dans la BDD
+INSERT INTO brand (`name`) VALUES
+('Chimay'),
+('Duvel'),
+('Kwak'),
+('Guinness'),
+('Ch''ti');
---
+++ 13-beerpdomysql/datas/ebc.sql
@@ -0,0 +1,11 @@
+-- EBC : Correspond à la couleur de bière
+-- 4 : Blonde : F8F753
+-- 26 : Ambré : BC6733
+-- 39 : Brune : 5D341A
+-- 57 : Noire : 0F0B0A
+
+INSERT INTO ebc (`code`, color) VALUES
+(4, 'F8F753'),
+(26, 'BC6733'),
+(39, '5D341A'),
+(57, '0F0B0A');
