9269a7473cf2c9e360ee9aee33c359bae80134d4
Matthieu Mota <matthieumota@gmail.com>
2018-05-31 12:32:50 +0200

    Structure

---
+++ 13-beerpdomysql/index.php
@@ -0,0 +1,14 @@
+<?php
+
+// Inclure le fichier config/database.php
+// Inclure le fichier partials/header.php
+require('partials/header.php'); ?>
+
+<!-- Le contenu de la page -->
+<div class="container pt-5">
+    <h1>Bienvenue sur Beer PDO</h1>
+</div>
+
+<?php
+// Inclure le fichier partials/footer.php
+require('partials/footer.php');
---
+++ 13-beerpdomysql/partials/header.php
@@ -0,0 +1,39 @@
+<?php
+// Configuration de PDO pour la base de données
+// On utilise la notation en absolue pour se repérer
+require(__DIR__.'/../config/database.php');
+?>
+
+<!doctype html>
+<html lang="fr">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <!-- Le menu -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <a class="navbar-brand" href="#">Beer PDO</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="index.php">Accueil</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="beer_list.php">Les Bières</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
