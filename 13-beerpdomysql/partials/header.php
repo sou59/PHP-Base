209800d3d96733c2e26d65eb7cb82e839cf8826d
Sou59 <sz5962@gmail.com>
2018-06-01 22:40:14 +0200

    MAJ

--- 13-beerpdomysql/partials/header.php
+++ 13-beerpdomysql/partials/header.php
@@ -1,66 +1,52 @@
-<?php 
+<?php
 // Configuration de PDO pour la base de données
 // On utilise la notation en absolue pour se repérer
 require(__DIR__.'/../config/database.php');
 ?>
 
 <!doctype html>
-<html lang="en">
+<html lang="fr">
   <head>
+    <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <!-- <link rel="icon" href="../../../../favicon.ico"> -->
 
-    <title>Les bières</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
 
-    <!-- Bootstrap core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    
-
-    <!-- Custom styles for this template -->
-    <link href="https://fonts.googleapis.com/css?family=Playfair+Display:700,900" rel="stylesheet">
-    <link href="css/script.css" rel="stylesheet">
+    <title>Hello, world!</title>
   </head>
-
   <body>
+    <!-- Le menu -->
+    <header>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <a class="navbar-brand" href="index.php">
+                <img class="logo" src="img/logo.png" alt="Beer PDO">
+                Beer PDO
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-  <header>
-    <!-- Fixed navbar -->
-    <nav class="navbar navbar-expand-md navbar-dark  bg-dark">
-      <a class="navbar-brand pl-auto" href="index.php"><img class="logo" src="img/beer.png"> Beer PDO</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarCollapse">
-        <?php
-        // Permet de récupérer le nom de la page sur laquelle on se trouve
-          $page = basename($_SERVER['REQUEST_URI'], '.php');  ?>
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item active <?php echo ($page == 'index') ? 'active' : '' ?>">
-              <a class="nav-link" href="index.php">Accueil <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item <?php echo ($page == 'beer_list') ? 'active' : '' ?>">
-              <a class="nav-link" href="beer_list.php">Les bières</a>
-            </li>
-            <li class="nav-item <?php echo ($page == 'beer_add') ? 'active' : '' ?>">
-              <a class="nav-link" href="beer_add.php">Ajouter une bières</a>
-            </li>
-          </ul>
-          <form class="form-inline mt-2 mt-md-0">
-            <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-          </form>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <?php
+                // Permet de récupérer le nom de la page sur laquelle on se trouve
+                $page = basename($_SERVER['REQUEST_URI'], '.php'); ?>
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item <?php echo ($page == 'index') ? 'active' : '' ?>">
+                        <a class="nav-link" href="index.php">Accueil</a>
+                    </li>
+                    <li class="nav-item <?php echo ($page == 'beer_list') ? 'active' : '' ?>">
+                        <a class="nav-link" href="beer_list.php">Les Bières</a>
+                    </li>
+                    <li class="nav-item <?php echo ($page == 'beer_add') ? 'active' : '' ?>">
+                        <a class="nav-link" href="beer_add.php">Ajouter une bière</a>
+                    </li>
+                </ul>
+            </div>
         </div>
+        <?php var_dump(basename($_SERVER['REQUEST_URI'], '.php')); ?>
     </nav>
-
-  <?php
-  $page = basename($_SERVER['REQUEST_URI'], '.php');
-  // affiche : C:\xampp\htdocs\PHP-Base\13-beerpdomysql\partials\header.php:55:string '/PHP-Base/13-beerpdomysql/beer_list.php' (length=39)
-  // selon page 
-  //basename('/PHP-Base/13-beerpdomysql/beer_list.php');
-  ?>
-  
-  </header>
+</header>
