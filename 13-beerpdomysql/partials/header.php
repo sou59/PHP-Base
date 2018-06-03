eb2a738475b077c58b8aa3c51393032042cf364e
Sou59 <sz5962@gmail.com>
2018-06-03 19:18:48 +0200

    modif bootstrap

--- 13-beerpdomysql/partials/header.php
+++ 13-beerpdomysql/partials/header.php
@@ -12,41 +12,38 @@ require(__DIR__.'/../config/database.php');
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
 
-    <title>Hello, world!</title>
-  </head>
-  <body>
+    <title>Les bières</title>
+</head>
+<body>
     <!-- Le menu -->
-    <header>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container">
-            <a class="navbar-brand" href="index.php">
-                <img class="logo" src="img/logo.png" alt="Beer PDO">
-                Beer PDO
-            </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+<header>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark" id="ma-nav">
+        <a class="navbar-brand" href="index.php">
+            <img class="logo" src="img/logo.png" alt="Beer PDO">Beer PDO</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <?php
-                // Permet de récupérer le nom de la page sur laquelle on se trouve
-                $page = basename($_SERVER['REQUEST_URI'], '.php'); ?>
-                <ul class="navbar-nav ml-auto">
-                    <li class="nav-item <?php echo ($page == 'index') ? 'active' : '' ?>">
-                        <a class="nav-link" href="index.php">Accueil</a>
-                    </li>
-                    <li class="nav-item <?php echo ($page == 'beer_list') ? 'active' : '' ?>">
-                        <a class="nav-link" href="beer_list.php">Les Bières</a>
-                    </li>
-                    <li class="nav-item <?php echo ($page == 'beer_add') ? 'active' : '' ?>">
-                        <a class="nav-link" href="beer_add.php">Ajouter une bière</a>
-                    </li>
-                </ul>
-            </div>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <?php
+            // Permet de récupérer le nom de la page sur laquelle on se trouve
+            $page = basename($_SERVER['REQUEST_URI'], '.php'); ?>
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item <?php echo ($page == 'index') ? 'active' : '' ?>">
+                    <a class="nav-link" href="index.php">Accueil</a>
+                </li>
+                <li class="nav-item <?php echo ($page == 'beer_list') ? 'active' : '' ?>">
+                    <a class="nav-link" href="beer_list.php">Les Bières</a>
+                </li>
+                <li class="nav-item <?php echo ($page == 'beer_add') ? 'active' : '' ?>">
+                    <a class="nav-link" href="beer_add.php">Ajouter une bière</a>
+                </li>
+            </ul>
         </div>
+        
         <?php var_dump(basename($_SERVER['REQUEST_URI'], '.php')); ?>
     </nav>
 </header>
