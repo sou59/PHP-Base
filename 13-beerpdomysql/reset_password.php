6bc58b19f4b9bb2e1ac8f43520786093e4f2ab59
Sou59 <sz5962@gmail.com>
2018-06-08 20:04:25 +0200

    MAJ reset

--- 13-beerpdomysql/reset_password.php
+++ 13-beerpdomysql/reset_password.php
@@ -1,15 +1,16 @@
 <?php
-require(__DIR__.'/partials/header.php');
+// Inclure le fichier partials/header.php
+require('partials/header.php');
 
-
-if(empty($_GET['id']) || empty($_GET['token']) || !isValidToken($_GET['token'], $_GET['id'])) {
-    echo 'Le token est non valide';
-    exit();
-
+// Vérifie que l'id et le token correspondent
+if ( empty($_GET['id']) || empty($_GET['token']) || !isValidToken($_GET['token'], $_GET['id']) ) {
+    echo 'Le token n\'est pas valide';
+    exit(); // On arrête le script PHP
 }
 ?>
 
 
+<!-- Le contenu de la page -->
 <div class="container pt-5">
     <h1>Redéfinir mon mot de passe</h1>
 
@@ -17,46 +18,34 @@ if(empty($_GET['id']) || empty($_GET['token']) || !isValidToken($_GET['token'], 
         if (!empty($_POST)) {
             $password = $_POST['password'];
             $cfpassword = $_POST['cfpassword'];
-
             $error = null;
-
             if (empty($password) || $password != $cfpassword) {
-                $query = $db->prepare('SELECT * FROM user id = :id');
+                $error = 'Le mot de passe n\'est pas valide';
+            }
+            if (!$error) {
+                $query = $db->prepare('UPDATE user SET password = :password WHERE id = :id');
                 $query->bindValue(':id', $_GET['id'], PDO::PARAM_INT);
-                $query->bindValue(':password', passeword_hash($password), PASSWORD_DEFAULT, PDO::PARAM_STR);
-                if($query->execute()) {
+                $query->bindValue(':password', password_hash($password, PASSWORD_DEFAULT), PDO::PARAM_INT);
+                if ($query->execute()) {
                     echo 'Le mot de passe a été changé.';
                 }
-
-
-
             }
-
         }
-
     ?>
 
-
     <form method="POST">
-    <div class="form-group">
-        <label for="password">Nouveau mot de passe</label>
-        <input type="password" name="password" class="form-control" id="password">
-      </div>
-
-      <div class="form-group">
-        <label for="cfpassword">Comfirmer le nouveau mot de passe</label>
-        <input type="password" name="cfpassword" class="form-control" id="cfpassword">
-      </div>
-
-      <button class="btn btn-primary">Modifier mon mot de passe</button>
+        <div class="form-group">
+            <label for="password">Nouveau mot de passe</label>
+            <input type="password" name="password" id="password" class="form-control">
+        </div>
+        <div class="form-group">
+            <label for="cfpassword">Confirmer le nouveau mot de passe</label>
+            <input type="password" name="cfpassword" id="cfpassword" class="form-control">
+        </div>
+        <button class="btn btn-primary">Modifier mon mot de passe</button>
     </form>
 </div>
 
-
-
-
-
 <?php
-
-require(__DIR__.'/partials/footer.php');
-?>
+// Inclure le fichier partials/footer.php
+require('partials/footer.php');
