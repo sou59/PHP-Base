248ae5d4ae0abb4a03cea96ef30c1f518946fa01
Sou59 <sz5962@gmail.com>
2018-06-11 20:07:49 +0200

    img

--- 17-revision/serie/datas/brand.sql
+++
@@ -1,7 +0,0 @@
--- Fichier nous permettra d'ajouter les marques
-INSERT INTO brand (`name`) VALUES 
-('Chimay'),
-('Duvel'),
-('Kwak'),
-('Guinness'),
-('Ch''ti');
--- 17-revision/serie/datas/tvshow.sql
+++ 17-revision/serie/datas/tvshow.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
+-- version 4.7.9
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le :  lun. 11 juin 2018 à 14:54
--- Version du serveur :  10.1.26-MariaDB
--- Version de PHP :  7.1.9
+-- Généré le :  lun. 11 juin 2018 à 20:03
+-- Version du serveur :  10.1.31-MariaDB
+-- Version de PHP :  7.2.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -31,9 +31,9 @@ SET time_zone = "+00:00";
 CREATE TABLE `episode` (
   `id` int(11) NOT NULL,
   `number` int(11) NOT NULL,
-  `title` varchar(255) DEFAULT NULL,
-  `release_at` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `title` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
+  `released_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -44,9 +44,9 @@ CREATE TABLE `episode` (
 CREATE TABLE `season` (
   `id` int(11) NOT NULL,
   `number` int(11) NOT NULL,
-  `title` varchar(255) DEFAULT NULL,
+  `title` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
   `released_at` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -56,52 +56,23 @@ CREATE TABLE `season` (
 
 CREATE TABLE `showtv` (
   `id` int(11) NOT NULL,
-  `title` varchar(255) NOT NULL,
-  `category` varchar(255) NOT NULL,
-  `cover` varchar(255) DEFAULT NULL,
-  `synopsis` text NOT NULL,
+  `title` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `category` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `cover` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
+  `synopsis` text CHARACTER SET utf8 NOT NULL,
   `released_at` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `showtv`
 --
 
 INSERT INTO `showtv` (`id`, `title`, `category`, `cover`, `synopsis`, `released_at`) VALUES
-(1, 'L\'Arme fatal. Saison 1', 'Action', '', 'Roger Murtaugh, policier de retour au travail après un infarctus, fait la connaissance de son nouveau coéquipier, Martin Riggs, instable suite au décès de sa femme. Ils élucident ensemble le meurtre d’un ancien Navy SEAL, Ramon Alvarez.', '2016-05-21'),
-(2, 'L\'Arme fatal. Saison 2', 'Action', 'img/-.jpg', 'Roger Murtaugh, policier de retour au travail après un infarctus, fait la connaissance de son nouveau coéquipier, Martin Riggs, instable suite au décès de sa femme. Ils élucident ensemble le meurtre d’un ancien Navy SEAL, Ramon Alvarez.', '2016-05-21'),
-(3, 'L\'Arme fatal. Saison 2', 'Action', 'img/-.jpg', 'Roger Murtaugh, policier de retour au travail après un infarctus, fait la connaissance de son nouveau coéquipier, Martin Riggs, instable suite au décès de sa femme. Ils élucident ensemble le meurtre d’un ancien Navy SEAL, Ramon Alvarez.', '0000-00-00'),
-(4, 'L\'Arme fatal. Saison 2', 'Action', 'img/-larme-fatal.-saison-2.jpg', 'Roger Murtaugh, policier de retour au travail après un infarctus, fait la connaissance de son nouveau coéquipier, Martin Riggs, instable suite au décès de sa femme. Ils élucident ensemble le meurtre d’un ancien Navy SEAL, Ramon Alvarez.', '0000-00-00'),
-(5, 'L\'arme fatal saison 3', 'Action', 'img/-larme-fatal-saison-3.jpg', 'Roger Murtaugh, policier de retour au travail après un infarctus, fait la connaissance de son nouveau coéquipier, Martin Riggs, instable suite au décès de sa femme. Ils élucident ensemble le meurtre d’un ancien Navy SEAL, Ramon Alvarez.', '0000-00-00'),
-(6, 'L\'arme fatal saison 3', 'Action', 'img/-larme-fatal-saison-3.jpg', 'Après le braquage d’un fourgon blindé transportant l’argent de dispensaires, Roger et Martin enquêtent sur une sombre histoire de drogue sur fond d’argent du cartel. Si les voleurs initiaux étaient les propriétaires du dispensaire eux-mêmes, c’est en réalité le conducteur du fourgon, ancien agent instructeur de Roger, qui a récupéré l’argent. ', '0000-00-00'),
-(7, 'L\'arme fatal saison 3', 'Action', 'img/-larme-fatal-saison-3.jpg', 'Après le braquage d’un fourgon blindé transportant l’argent de dispensaires, Roger et Martin enquêtent sur une sombre histoire de drogue sur fond d’argent du cartel. Si les voleurs initiaux étaient les propriétaires du dispensaire eux-mêmes, c’est en réalité le conducteur du fourgon, ancien agent instructeur de Roger, qui a récupéré l’argent. ', '0000-00-00'),
-(8, 'L\'arme fatal saison 3', 'Action', 'img/-larme-fatal-saison-3.jpg', 'Après le braquage d’un fourgon blindé transportant l’argent de dispensaires, Roger et Martin enquêtent sur une sombre histoire de drogue sur fond d’argent du cartel. Si les voleurs initiaux étaient les propriétaires du dispensaire eux-mêmes, c’est en réalité le conducteur du fourgon, ancien agent instructeur de Roger, qui a récupéré l’argent. ', '0000-00-00'),
-(9, 'L\'arme fatal saison 3', 'Action', 'img/-larme-fatal-saison-3.jpg', 'Après le braquage d’un fourgon blindé transportant l’argent de dispensaires, Roger et Martin enquêtent sur une sombre histoire de drogue sur fond d’argent du cartel. Si les voleurs initiaux étaient les propriétaires du dispensaire eux-mêmes, c’est en réalité le conducteur du fourgon, ancien agent instructeur de Roger, qui a récupéré l’argent. ', '0000-00-00'),
-(10, 'larme-fatal-saison-3', 'Action', 'img/-larme-fatal-saison-3.jpg', 'Après le braquage d’un fourgon blindé transportant l’argent de dispensaires, Roger et Martin enquêtent sur une sombre histoire de drogue sur fond d’argent du cartel. Si les voleurs initiaux étaient les propriétaires du dispensaire eux-mêmes, c’est en réalité le conducteur du fourgon, ancien agent instructeur de Roger, qui a récupéré l’argent. ', '0000-00-00'),
-(11, 'L\'arme fatale. Saison 12', 'Action', 'img/-larme-fatale.-saison-12.jpg', 'Machine a tué machin.', '0000-00-00'),
-(12, 'L\'arme fatale. Saison 13', 'Action', 'img/-larme-fatale.-saison-13.jpg', 'Après le braquage d’un fourgon blindé transportant l’argent de dispensaires, Roger et Martin enquêtent sur une sombre histoire de drogue sur fond d’argent du cartel. Si les voleurs initiaux étaient les propriétaires du dispensaire eux-mêmes, c’est en réalité le conducteur du fourgon, ancien agent instructeur de Roger, qui a récupéré l’argent. ', '0000-00-00'),
-(13, 'L\'arme fatale. Saison 22', 'Action', 'img/larme-fatale.-saison-22.jpg', 'Après le braquage d’un fourgon blindé transportant l’argent de dispensaires, Roger et Martin enquêtent sur une sombre histoire de drogue sur fond d’argent du cartel. Si les voleurs initiaux étaient les propriétaires du dispensaire eux-mêmes, c’est en réalité le conducteur du fourgon, ancien agent instructeur de Roger, qui a récupéré l’argent. ', '0000-00-00'),
-(14, 'larme-fatale.-saison-22', 'Action', 'img/larme-fatale.-saison-22.', 'Après le braquage d’un fourgon blindé transportant l’argent de dispensaires, Roger et Martin enquêtent sur une sombre histoire de drogue sur fond d’argent du cartel. Si les voleurs initiaux étaient les propriétaires du dispensaire eux-mêmes, c’est en réalité le conducteur du fourgon, ancien agent instructeur de Roger, qui a récupéré l’argent. ', '0000-00-00'),
-(15, 'L\'Arme fatal. Saison 1', 'Action', 'img/larme-fatal.-saison-1.', 'Après le braquage d’un fourgon blindé transportant l’argent de dispensaires, Roger et Martin enquêtent sur une sombre histoire de drogue sur fond d’argent du cartel. Si les voleurs initiaux étaient les propriétaires du dispensaire eux-mêmes, c’est en réalité le conducteur du fourgon, ancien agent instructeur de Roger, qui a récupéré l’argent. ', '0000-00-00'),
-(16, 'L\'Arme fatal. Saison 1', 'Action', 'img/larme-fatal.-saison-1.', 'Après le braquage d’un fourgon blindé transportant l’argent de dispensaires, Roger et Martin enquêtent sur une sombre histoire de drogue sur fond d’argent du cartel. Si les voleurs initiaux étaient les propriétaires du dispensaire eux-mêmes, c’est en réalité le conducteur du fourgon, ancien agent instructeur de Roger, qui a récupéré l’argent. ', '0000-00-00'),
-(17, 'L\'Arme fatal. Saison 1', 'Action', 'img/larme-fatal.-saison-1.', 'Après le braquage d’un fourgon blindé transportant l’argent de dispensaires, Roger et Martin enquêtent sur une sombre histoire de drogue sur fond d’argent du cartel. Si les voleurs initiaux étaient les propriétaires du dispensaire eux-mêmes, c’est en réalité le conducteur du fourgon, ancien agent instructeur de Roger, qui a récupéré l’argent. ', '0000-00-00'),
-(18, 'L\'Arme fatal. Saison 1', 'Action', 'img/larme-fatal.-saison-1.', 'Après le braquage d’un fourgon blindé transportant l’argent de dispensaires, Roger et Martin enquêtent sur une sombre histoire de drogue sur fond d’argent du cartel. Si les voleurs initiaux étaient les propriétaires du dispensaire eux-mêmes, c’est en réalité le conducteur du fourgon, ancien agent instructeur de Roger, qui a récupéré l’argent. ', '0000-00-00'),
-(19, 'L\'Arme fatal. Saison 1', 'Action', 'img/larme-fatal.-saison-1.', 'Après le braquage d’un fourgon blindé transportant l’argent de dispensaires, Roger et Martin enquêtent sur une sombre histoire de drogue sur fond d’argent du cartel. Si les voleurs initiaux étaient les propriétaires du dispensaire eux-mêmes, c’est en réalité le conducteur du fourgon, ancien agent instructeur de Roger, qui a récupéré l’argent. ', '0000-00-00'),
-(20, 'L\'Arme fatal. Saison 1', 'Action', 'img/larme-fatal.-saison-1.', 'Après le braquage d’un fourgon blindé transportant l’argent de dispensaires, Roger et Martin enquêtent sur une sombre histoire de drogue sur fond d’argent du cartel. Si les voleurs initiaux étaient les propriétaires du dispensaire eux-mêmes, c’est en réalité le conducteur du fourgon, ancien agent instructeur de Roger, qui a récupéré l’argent. ', '0000-00-00'),
-(21, 'L\'Arme fatal. Saison 1', 'Action', 'img/larme-fatal.-saison-1.', 'Après le braquage d’un fourgon blindé transportant l’argent de dispensaires, Roger et Martin enquêtent sur une sombre histoire de drogue sur fond d’argent du cartel. Si les voleurs initiaux étaient les propriétaires du dispensaire eux-mêmes, c’est en réalité le conducteur du fourgon, ancien agent instructeur de Roger, qui a récupéré l’argent. ', '0000-00-00'),
-(22, 'L\'Arme fatal. Saison 1', 'Action', 'img/larme-fatal.-saison-1.', 'Après le braquage d’un fourgon blindé transportant l’argent de dispensaires, Roger et Martin enquêtent sur une sombre histoire de drogue sur fond d’argent du cartel. Si les voleurs initiaux étaient les propriétaires du dispensaire eux-mêmes, c’est en réalité le conducteur du fourgon, ancien agent instructeur de Roger, qui a récupéré l’argent. ', '0000-00-00'),
-(23, 'L\'Arme fatal. Saison 1', 'Action', 'img/larme-fatal.-saison-1.', 'Après le braquage d’un fourgon blindé transportant l’argent de dispensaires, Roger et Martin enquêtent sur une sombre histoire de drogue sur fond d’argent du cartel. Si les voleurs initiaux étaient les propriétaires du dispensaire eux-mêmes, c’est en réalité le conducteur du fourgon, ancien agent instructeur de Roger, qui a récupéré l’argent. ', '0000-00-00'),
-(24, 'L\'Arme fatal. Saison 1', 'Action', 'img/larme-fatal.-saison-1.', 'Après le braquage d’un fourgon blindé transportant l’argent de dispensaires, Roger et Martin enquêtent sur une sombre histoire de drogue sur fond d’argent du cartel. Si les voleurs initiaux étaient les propriétaires du dispensaire eux-mêmes, c’est en réalité le conducteur du fourgon, ancien agent instructeur de Roger, qui a récupéré l’argent. ', '0000-00-00'),
-(25, 'L\'Arme fatal. Saison 1', 'Action', 'img/larme-fatal.-saison-1.', 'Après le braquage d’un fourgon blindé transportant l’argent de dispensaires, Roger et Martin enquêtent sur une sombre histoire de drogue sur fond d’argent du cartel. Si les voleurs initiaux étaient les propriétaires du dispensaire eux-mêmes, c’est en réalité le conducteur du fourgon, ancien agent instructeur de Roger, qui a récupéré l’argent. ', '0000-00-00'),
-(26, 'L\'arme fatale. Saison 42', 'Horreur', 'img/larme-fatale.-saison-42.jpg', 'Machin a tué truc.', '2016-05-12'),
-(27, 'L\'arme fatale. Saison 42', 'Horreur', 'img/larme-fatale.-saison-42.jpg', 'Machin a tué truc.', '2016-05-12'),
-(28, 'L\'arme fatale. Saison 42', 'Horreur', 'img/larme-fatale.-saison-42.jpg', 'Machin a tué truc.', '2016-05-12'),
-(29, 'L\'arme fatale. Saison 42', 'Horreur', 'img/larme-fatale.-saison-42.jpg', 'Machin a tué truc.', '2016-05-12'),
-(30, 'L\'arme fatale. Saison 42', 'Horreur', 'img/larme-fatale.-saison-42.jpg', 'Machin a tué truc.', '2016-05-12'),
-(31, 'L\'Arme fatal. Saison 1', 'Action', 'img/larme-fatal.-saison-1.', 'Roger Murtaugh, policier de retour au travail après un infarctus, fait la connaissance de son nouveau coéquipier, Martin Riggs, instable suite au décès de sa femme. Ils élucident ensemble le meurtre d’un ancien Navy SEAL, Ramon Alvarez.', '0000-00-00'),
-(32, 'larme-fatal.-saison-1', 'Action', 'img/larme-fatal.-saison-1.', 'Roger Murtaugh, policier de retour au travail après un infarctus, fait la connaissance de son nouveau coéquipier, Martin Riggs, instable suite au décès de sa femme. Ils élucident ensemble le meurtre d’un ancien Navy SEAL, Ramon Alvarez.', '2016-05-12'),
-(33, 'L\'Arme fatal. Saison 1', 'Action', 'img/larme-fatal.-saison-1.', 'Roger Murtaugh, policier de retour au travail après un infarctus, fait la connaissance de son nouveau coéquipier, Martin Riggs, instable suite au décès de sa femme. Ils élucident ensemble le meurtre d’un ancien Navy SEAL, Ramon Alvarez.', '2016-05-12'),
-(34, 'larme-fatal.-saison-1', 'Action', 'img/larme-fatal.-saison-1.', 'Roger Murtaugh, policier de retour au travail après un infarctus, fait la connaissance de son nouveau coéquipier, Martin Riggs, instable suite au décès de sa femme. Ils élucident ensemble le meurtre d’un ancien Navy SEAL, Ramon Alvarez.', '2016-05-21');
+(1, 'L\'Arme fatale. Saison 1', 'Action', '', 'Veuf depuis la mort tragique de son ?pouse enceinte, Martin Riggs, flic texan et ancien de la Marine, prend un nouveau d?part ? Los Angeles. Il devient le co-?quipier du d?tective Roger Murtaugh, lequel a r?cemment subi une crise cardiaque b?nigne et doit ? tout prix ?viter les situations de stress. Entre l\'un prudent et l\'autre trop impr?visible, le duo de choc risque de faire des ?tincelles.', '0000-00-00'),
+(2, 'L\'Arme fatale. Saison 2', 'Action', '', 'Il devient le co-?quipier du d?tective Roger Murtaugh, lequel a r?cemment subi une crise cardiaque b?nigne et doit ? tout prix ?viter les situations de stress. Entre l\'un prudent et l\'autre trop impr?visible, le duo de choc risque de faire des ?tincelles.', '0000-00-00'),
+(3, 'L\'Arme fatale. Saison 2', 'Action', '', 'Veuf depuis la mort tragique de son ?pouse enceinte, Martin Riggs, flic texan et ancien de la Marine, prend un nouveau d?part ? Los Angeles. Il devient le co-?quipier du d?tective Roger Murtaugh, lequel a r?cemment subi une crise cardiaque b?nigne et doit ? tout prix ?viter les situations de stress. Entre l\'un prudent et l\'autre trop impr?visible, le duo de choc risque de faire des ?tincelles.', '0000-00-00'),
+(4, 'dsdfsdf', 'Action', '', 'tesdf', '0000-00-00'),
+(5, 'L\'arme fatale', 'Action', 'img/larme-fatale.jpg', '18 épisodes Diffusé sur FOX Saison produite en 2016', '2016-01-01');
 
 --
 -- Index pour les tables déchargées
@@ -117,7 +88,7 @@ ALTER TABLE `episode`
 -- Index pour la table `season`
 --
 ALTER TABLE `season`
-  ADD PRIMARY KEY (`id`);
+  ADD UNIQUE KEY `id` (`id`);
 
 --
 -- Index pour la table `showtv`
@@ -145,7 +116,7 @@ ALTER TABLE `season`
 -- AUTO_INCREMENT pour la table `showtv`
 --
 ALTER TABLE `showtv`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Contraintes pour les tables déchargées
